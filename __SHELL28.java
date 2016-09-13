65cf0ccaabedc67d5f4002656e5a78a6673e1290
HannahMcCarvill <Guest@stk-cs01-ws17.westlothian.sch.uk>
2016-09-13 16:47:19 +0100

    fileread

---
+++ __SHELL28.java
@@ -0,0 +1,11 @@
+
+public class __SHELL28 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/Guest/Documents/elevator");
+final WRITEFILE wRITEFIL1 = (WRITEFILE)__bluej_runtime_scope.get("wRITEFIL1");
+
+
+java.lang.String __bluej_param0 = "First Name,Surname/nHannah,Mccarvill/nBeatrice,Senkute";
+wRITEFIL1.writeCSVtable(__bluej_param0);
+
+}}
